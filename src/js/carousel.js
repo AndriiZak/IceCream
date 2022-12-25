4e5ed55f90e757c3b6e6d24500f4f4951eeca96f
Andrii <112751333+AndriiZak@users.noreply.github.com>
2022-12-26 01:34:16 +0200

    update

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -5,3 +5,11 @@ $('.your-class').slick({
   dots: true,
   arrows: false,
 });
+
+$('.your-class-slideshow').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false,
+});
