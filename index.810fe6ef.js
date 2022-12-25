cbfcdcb5eaf48371d28d914987d3f7264b73c17a
AndriiZak <112751333+AndriiZak@users.noreply.github.com>
2022-12-25 23:35:05 +0000

    Deploying to gh-pages from @ AndriiZak/IceCream@4e5ed55f90e757c3b6e6d24500f4f4951eeca96f 🚀

--- index.810fe6ef.js
+++
@@ -1,2 +0,0 @@
-$(".your-class").slick({infinite:!0,slidesToShow:1,slidesToScroll:1,dots:!0,arrows:!1});
-//# sourceMappingURL=index.810fe6ef.js.map
---
+++ index.f5da684b.js
@@ -0,0 +1,2 @@
+$(".your-class").slick({infinite:!0,slidesToShow:1,slidesToScroll:1,dots:!0,arrows:!1}),$(".your-class-slideshow").slick({infinite:!0,slidesToShow:1,slidesToScroll:1,dots:!0,arrows:!1});
+//# sourceMappingURL=index.f5da684b.js.map
